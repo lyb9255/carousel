ff8ccfeea1dc787f9e5498ebd06ce0896815881e
LYB <82506086+lyb9255@users.noreply.github.com>
2021-08-23 20:43:59 +0900

    img 사이즈가 바뀌어도 적용될수 있게 offsetWidth로 바꿈.

--- index.js
+++ index.js
@@ -5,48 +5,45 @@ const leftArrow = document.querySelector('.left-area .arrowwrapper')
 const rightArrow = document.querySelector('.right-area .arrowwrapper')
 
 const dots = document.querySelectorAll('.dot')
-const dot1 = document.querySelector('.dot1')
-const dot2 = document.querySelector('.dot2')
-const dot3 = document.querySelector('.dot3')
-const dot4 = document.querySelector('.dot4')
-const dot5 = document.querySelector('.dot5')
 
 let i = 1;
+const size = carouselImages[0].offsetWidth
 
 rightArrow.addEventListener('click', function () {
   i++;
-  carouselSlide.style.transform = 'translateX('+ (-600*i) + 'px';
+  
+  carouselSlide.style.transform = 'translateX('+ (-size*i) + 'px';
+  
   if (i === carouselImages.length) {
     i=0
-    carouselSlide.style.transform = 'translateX('+ (-600*i) + 'px';;
+    carouselSlide.style.transform = 'translateX('+ (-size*i) + 'px';;
   }
+  
+
 });
 
 leftArrow.addEventListener('click', function () {
+
   i--;
-  carouselSlide.style.transform = 'translateX('+ (-600*i) + 'px';
+
+  carouselSlide.style.transform = 'translateX('+ (-size*i) + 'px';
+  
   if(i === -1) {
     i = carouselImages.length -1
-    carouselSlide.style.transform = 'translateX('+ (-600*i) + 'px';;
+    carouselSlide.style.transform = 'translateX('+ (-size*i) + 'px';;
   }
+
+
 });
 
 dots.forEach(function (dot, index) {
+
   dot.addEventListener('click', function () {
-    carouselSlide.style.transform = 'translateX(' + (-600 * index) + 'px';;
-  });
-});
-
-
-
-
-dots.forEach(function (dot,index) {
-  carouselImages.forEach(function(image,order) {
-    if (index === order) {
-      dot.style.color = 'rgba(0,0,0,.75)'
-    } else {
-      dot.style.color = 'rgba(0,0,0,.3)'
-    }
+    carouselSlide.style.transform = 'translateX(' + (-size * index) + 'px';;
   });
 
 });
+
+
+
+
