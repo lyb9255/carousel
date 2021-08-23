8f0d3fd7f08967fd93983a7b7d6a2d7609fce517
LYB <82506086+lyb9255@users.noreply.github.com>
2021-08-24 03:13:33 +0900

    맨처음에 사진1 에서 사진3으로 바로 넘어가던거 수정

--- index.js
+++ index.js
@@ -1,53 +1,50 @@
-const carouselSlide = document.querySelector('.carousel-slider');
-const carouselImages = document.querySelectorAll('.carousel-slider img')
-
+const carouselSlides = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img')
 const leftArrow = document.querySelector('.left-area .arrowwrapper')
 const rightArrow = document.querySelector('.right-area .arrowwrapper')
 
 const dots = document.querySelectorAll('.dot')
 
 
-let i = 1;
-const size = carouselImages[0].offsetWidth
+let i = 0;
+const size = carouselImages[0].offsetWidth;
 
 
 
 rightArrow.addEventListener('click', function () {
-  console.log(i)
   i++;
-  
-  carouselSlide.style.transform = 'translateX('+ (-size*i) + 'px';
-  
+  carouselSlides.style.transform = 'translateX('+ (-size*i) + 'px';
   if (i === carouselImages.length) {
     i=0
-    carouselSlide.style.transform = 'translateX('+ (-size*i) + 'px';;
+    carouselSlides.style.transform = 'translateX('+ (-size*i) + 'px';;
   }
+  console.log(i)
 
 });
 
 
 
 leftArrow.addEventListener('click', function () {
-  console.log(i)
-  
   i--;
-
-  carouselSlide.style.transform = 'translateX('+ (-size*i) + 'px';
-  
+  carouselSlides.style.transform = 'translateX('+ (-size*i) + 'px';
   if(i === -1) {
     i = carouselImages.length-1
-    carouselSlide.style.transform = 'translateX('+ (-size*i) + 'px';;
+    carouselSlides.style.transform = 'translateX('+ (-size*i) + 'px';;
   }
 
+  console.log(i)
 
 });
+
+
 
 dots.forEach(function(dot, index) {
 
   dot.addEventListener('click', function () {
-    carouselSlide.style.transform = 'translateX(' + (-size * index) + 'px';;
+    carouselSlides.style.transform = 'translateX(' + (-size * index) + 'px';;
     i=index;
   });
-
+ 
 });
+
 
