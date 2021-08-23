a17b2c81d430cac238201c45644ea8654183726f
LYB <82506086+lyb9255@users.noreply.github.com>
2021-08-23 19:56:27 +0900

    Dot nav, loop 추가

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
-const carouselSlider = document.querySelector('.carousel-slider');
+const carouselSlide = document.querySelector('.carousel-slider');
 const carouselImages = document.querySelectorAll('.carousel-slider img')
 
 const leftArrow = document.querySelector('.left-area .arrowwrapper')
 const rightArrow = document.querySelector('.right-area .arrowwrapper')
 
-let i = 1;
-const size = carouselImages[0].clientWidth;
+const dots = document.querySelectorAll('.dot')
+const dot1 = document.querySelector('.dot1')
+const dot2 = document.querySelector('.dot2')
+const dot3 = document.querySelector('.dot3')
+const dot4 = document.querySelector('.dot4')
+const dot5 = document.querySelector('.dot5')
 
+let i = 1;
 
 rightArrow.addEventListener('click', function () {
   i++;
-  carouselSlider.style.transform = 'translateX('+ (-600*i) + 'px';
+  carouselSlide.style.transform = 'translateX('+ (-600*i) + 'px';
+  if (i === carouselImages.length) {
+    i=0
+    carouselSlide.style.transform = 'translateX('+ (-600*i) + 'px';;
+  }
 });
 
 leftArrow.addEventListener('click', function () {
   i--;
-  carouselSlider.style.transform = 'translateX('+ (-600*i) + 'px';
+  carouselSlide.style.transform = 'translateX('+ (-600*i) + 'px';
+  if(i === -1) {
+    i = carouselImages.length -1
+    carouselSlide.style.transform = 'translateX('+ (-600*i) + 'px';;
+  }
+});
+
+dots.forEach(function (dot, index) {
+  dot.addEventListener('click', function () {
+    carouselSlide.style.transform = 'translateX(' + (-600 * index) + 'px';;
+  });
+});
+
+
+
+
+dots.forEach(function (dot,index) {
+  carouselImages.forEach(function(image,order) {
+    if (index === order) {
+      dot.style.color = 'rgba(0,0,0,.75)'
+    } else {
+      dot.style.color = 'rgba(0,0,0,.3)'
+    }
+  });
+
 });
